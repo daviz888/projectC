ce3a52cc92253d445a41e5cde729687899072864
Dopacs <ayos888@gmail.com>
2018-11-14 20:46:46 +0300

    new files

---
+++ ex202.c
@@ -0,0 +1,10 @@
+// Program 2.2 Using variable
+#include <stdio.h>
+
+int main(void)
+{
+  int salary;
+  salary = 1000;
+  printf("My salary is %d\n", salary);
+  return 0;
+}
---
+++ ex203.c
@@ -0,0 +1,17 @@
+// Program 2.3 Using more variables
+
+#include <stdio.h>
+
+int main()
+{
+    int brothers;
+    int brides;
+
+    brothers = 7;
+    brides = 7;
+
+    // Display some output.
+    printf("%d brides for %d brothers\n", brides, brothers);
+
+    return 0;
+}
